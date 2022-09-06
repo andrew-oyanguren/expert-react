175c4f7a45ee177eeabba373f7daeff35e4ede0e
andrew-oyanguren <aaoyanguren@outlook.com>
2022-09-05 22:44:09 -0700

    added styles to header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,9 @@
+import { StyledHeader } from "./Header.styles";
+
 export default function Header() {
   return (
-    <>
+    <StyledHeader>
       <h1>Header</h1>
-    </>
+    </StyledHeader>
   );
 }
