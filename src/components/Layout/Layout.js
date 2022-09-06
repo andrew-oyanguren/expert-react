f681d88d0d524f6d4e119f073d7adfcc4b26b3b6
andrew-oyanguren <aaoyanguren@outlook.com>
2022-09-05 22:55:57 -0700

    added styles to Layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
+import { StyledLayout } from "./Layout.styles";
 import Header from "../Header/Header";
 
 export default function Layout({ children }) {
   return (
-    <>
+    <StyledLayout>
       <Header />
       {children}
-    </>
+    </StyledLayout>
   );
 }
