d200f4a1d992c62d7fb6a8eceb811911bf8912e3
andrew-oyanguren <aaoyanguren@outlook.com>
2022-09-05 23:10:51 -0700

    imported and used displayed Navigation component in the Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,12 @@
 import { StyledHeader } from "./Header.styles";
 
+import Navigation from "../Navigation/Navigation";
+
 export default function Header() {
   return (
     <StyledHeader>
       <h1>Header</h1>
+      <Navigation />
     </StyledHeader>
   );
 }
